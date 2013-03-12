ae7c50b0235272fc0f3fc2ab17dc0d3d5c59925e
Santiago Suarez Ordoñez <santiycr@gmail.com>
2013-03-11 18:39:02 -0700

    start making the assumption this is running in TravisCI, stop installing oracle's java, openjdk is good enough

--- travis-scripts/sauce_connect_setup.sh
+++ travis-scripts/sauce_connect_setup.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
+# Setup and start Sauce Connect for your TravisCI build
+# This script requires your .travis.yml to set the following two private env variables:
+# SAUCE_USERNAME
+# SAUCE_ACCESS_KEY
+# Follow the steps at https://saucelabs.com/opensource/travis to set that up.
+
 CONNECT_URL="http://saucelabs.com/downloads/Sauce-Connect-latest.zip"
 STARTUP_TIMEOUT=90
 CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
 CONNECT_DOWNLOAD="Sauce_Connect.zip"
 READY_FILE="connect-ready-$RANDOM"
 
-if [ -n "$TRAVIS" ] && [ -n "$TRAVIS_JOB_NUMBER" ]; then
-    if [ -z $(which java) ]; then
-        # If running on travis, install Java
-        echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-        echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-        sudo add-apt-repository -y ppa:webupd8team/java
-        sudo apt-get update -qq
-        sudo apt-get install -qq oracle-java7-installer
-    fi
-    # If running on travis, use a tunnel identifier
-    TUNNEL_IDENTIFIER="--tunnel-identifier $TRAVIS_JOB_NUMBER"
-else
-    TUNNEL_IDENTIFIER=""
-fi
-
 # Get Connect and start it
 mkdir -p $CONNECT_DIR
 cd $CONNECT_DIR
 curl $CONNECT_URL > $CONNECT_DOWNLOAD
 unzip $CONNECT_DOWNLOAD
+rm $CONNECT_DOWNLOAD
 java -jar Sauce-Connect.jar --readyfile $READY_FILE \
-    $TUNNEL_IDENTIFIER \
+    --tunnel-identifier $TRAVIS_JOB_NUMBER \
     $SAUCE_USERNAME $SAUCE_ACCESS_KEY &
 
 # Wait for Connect to be ready before exiting
