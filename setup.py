06cb869ad5a329afbe59d61df36a693eac6e23fa
Santiago Suarez Ordoñez <santiycr@gmail.com>
2017-05-24 14:41:59 -0700

    moving to flask instead of google app engine

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cssify',
+      version='1.0',
+      description='Convert XPATH locators to CSS',
+      author='Santiago Suarez Ordonez',
+      author_email='santiycr@gmail.com',
+      url='http://cssify.appspot.com/',
+      packages=['cssify'],
+      install_requires=['flask'],
+      )
